692e1952a2f5e0911b656b946aae6679cf804ef7
Wilson R <wdramirez@espe.edu.ec>
2022-07-19 17:00:19 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates/layouts')
+app._static_folder = os.path.abspath("templates/static/")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
