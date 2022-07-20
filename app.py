5b13d2fba21de6a81aaaa190269ed090aaf2a8c2
Wilson R <wdramirez@espe.edu.ec>
2022-07-19 20:43:30 -0500

    Final

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-import os
-from flask import Flask, render_template
+import os #importacion de la biblioteca/modulo so para las acciones del sistema operativo
+from flask import Flask, render_template #importacion de Flask para nuestra aplicacion web
 
-app = Flask(__name__, template_folder='templates/layouts')
-app._static_folder = os.path.abspath("templates/static/")
+app = Flask(__name__, template_folder='templates/layouts') #variable que guardara nuestro archivos html
+app._static_folder = os.path.abspath("templates/static/") # directorio de nuestro archivos HTML
 
-@app.route('/')
+@app.route('/') #pagina principal
 def index():
-    return render_template('index.html')
+    return render_template('Inicio.html') #renderizado de la pagina principal
 
 if __name__ == '__main__':
     app.run(debug=True)
--- templates/static/js/JavaScript.js
+++ templates/static/js/JavaScript.js
@@ -1,24 +1,24 @@
 
-$(function() {
+$(function() { //Inicio de la funcion que contendra nuestras validaciones
   
-  $("form[name='registration']").validate({
+  $("form[name='registration']").validate({ //relacion con nuestro formulario en HTML
    
-    rules: {
+    rules: { // Campos/Id a revisar e intrucciones a cumplir 
       
-      primerNombre:{
+      primerNombre:{ //id del campo nombre con la instruccion de ser oligatorio para el formulario
         required: true
       },
 
-      apellido: {
+      apellido: { //id del campo apellido con la instruccion de ser oligatorio para el formulario
         required: true
       },
 
-      edad:{
+      edad:{ //id del campo edad con la instruccion de ser oligatorio para el formulario ademas de mayor a 18
           required: true,
           min:18
       },
 
-      email: {
+      email: { //id del campo email con la instruccion de ser oligatorio para el formulario ademas de ingresar una nomenclatura de correo real
         required: true,
         email: true
       },
@@ -26,18 +26,18 @@ $(function() {
      
     },
    
-    messages: {
+    messages: { //mensajes a mostrar su ni se cumplen o ingresan los datos requeridos en los campos
       primerNombre: {
-        required:"Por favor, introduzca su nombre"},
+        required:"Por favor, introduzca su nombre"}, //mensajes para el campo nombre
       apellido: {
-        required:"Por favor, introduzca su apellido",
+        required:"Por favor, introduzca su apellido",//mensajes para el campo apellido
       },
       edad: {
-        required:"Por favor, introduzca su edad",
-        min: "mayor de edad"
+        required:"Por favor, introduzca su edad",//mensajes para el campo edad
+        min: "mayor de edad"//mensajes para el campo edad si no se ingresa un numero igual o mayor a 18
       },
       email: {
-        required:"Por favor, introduce una dirección de correo",
+        required:"Por favor, introduce una dirección de correo",//mensajes para el campo nombre
       },
     },
     submitHandler: function(form) {
